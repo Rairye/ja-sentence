caf440a26663bb2b5a835666d7321216758affa4
Rairye <37942271+Rairye@users.noreply.github.com>
2021-10-10 05:57:59 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'ja_sentence',  
+  packages = ['ja_sentence'], 
+  version = '0.0.1', 
+  license='apache-2.0', 
+  description = 'Light-weight sentence tokenizer for Japanese.',
+  author = 'Rairye', 
+  url = 'https://github.com/Rairye/ja-sentence',   
+  download_url = 'https://github.com/Rairye/ja-sentence/archive/refs/tags/v0.0.1.tar.gz',
+  keywords = ['Japanese', 'Sentence', 'Tokenizer'],
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License', 
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
